51e5392019740b58370d054bbaaba316110cb58b
Andrey Nalimov <ae.nalimov@gmail.com>
2026-05-27 20:40:30 +0300

    Phase 5: wire feature-add-room presentation+ui to :shared:main (Koin)

--- shared/feature-add-room/impl/src/commonMain/kotlin/dev/nonoxy/feature/add_room/di/FeatureAddRoomImplModule.kt
+++ shared/feature-add-room/impl/src/commonMain/kotlin/dev/nonoxy/feature/add_room/di/FeatureAddRoomImplModule.kt
@@ -1,10 +1,21 @@
 package dev.nonoxy.feature.add_room.di
 
+import dev.nonoxy.common.coroutines.CoroutineDispatchers
+import dev.nonoxy.feature.add_room.api.store.AddRoomStore
+import dev.nonoxy.feature.add_room.impl.domain.AddRoomStoreFactory
 import dev.nonoxy.feature.add_room.presentation.OldAddRoomViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val featureAddRoomImplModule = module {
+
+    factory<AddRoomStore> {
+        AddRoomStoreFactory(
+            storeFactory = get(),
+            mainDispatcher = get<CoroutineDispatchers>().main,
+            roomsRepository = get(),
+        ).create()
+    }
 
     // TODO Task 7: remove together with OldAddRoomViewModel.
     viewModelOf(::OldAddRoomViewModel)
--- shared/main/src/commonMain/kotlin/dev/nonoxy/residetrack/di/Koin.kt
+++ shared/main/src/commonMain/kotlin/dev/nonoxy/residetrack/di/Koin.kt
@@ -3,6 +3,7 @@ package dev.nonoxy.residetrack.di
 import dev.nonoxy.common.di.commonModule
 import dev.nonoxy.core.database.di.coreDatabaseModule
 import dev.nonoxy.feature.add_room.di.featureAddRoomImplModule
+import dev.nonoxy.feature.add_room.presentation.di.featureAddRoomPresentationModule
 import dev.nonoxy.feature.manage_students.di.featureManageStudentsImplModule
 import dev.nonoxy.feature.rooms.impl.di.featureRoomsImplModule
 import dev.nonoxy.feature.rooms.presentation.di.featureRoomsPresentationModule
@@ -27,6 +28,7 @@ fun initKoin(appDeclaration: KoinAppDeclaration = {}) {
             featureRoomsPresentationModule,
 
             featureAddRoomImplModule,
+            featureAddRoomPresentationModule,
             featureManageStudentsImplModule,
         )
     }
