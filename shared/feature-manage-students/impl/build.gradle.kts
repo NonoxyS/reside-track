bad2639e3117a6ce11671da25f68d9063186c5f2
Andrey Nalimov <ae.nalimov@gmail.com>
2026-05-28 01:21:56 +0300

    Phase 5: feature-manage-students impl — add MVIKotlin domain (Executor + Reducer + parameterized StoreFactory)

--- shared/feature-manage-students/impl/build.gradle.kts
+++ shared/feature-manage-students/impl/build.gradle.kts
@@ -24,6 +24,7 @@ commonMainDependencies {
         projects.shared.commonUi,
         projects.shared.featureRooms.api,
         projects.shared.featureManageStudents.api,
+        projects.shared.coreMvikotlin,
     )
 }
 
