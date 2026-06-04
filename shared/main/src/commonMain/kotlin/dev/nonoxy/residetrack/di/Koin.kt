caf185dce84ed7b5c198e0495d26474798c70a77
Andrey Nalimov <ae.nalimov@gmail.com>
2026-06-04 20:37:12 +0300

    build: register coreRoomsModule, refactor green on android+ios+detekt

--- shared/main/src/commonMain/kotlin/dev/nonoxy/residetrack/di/Koin.kt
+++ shared/main/src/commonMain/kotlin/dev/nonoxy/residetrack/di/Koin.kt
@@ -3,6 +3,7 @@ package dev.nonoxy.residetrack.di
 import dev.nonoxy.residetrack.common.di.commonModule
 import dev.nonoxy.residetrack.core.database.di.coreDatabaseModule
 import dev.nonoxy.residetrack.core.initializer.di.coreInitializerModule
+import dev.nonoxy.residetrack.core.rooms.di.coreRoomsModule
 import dev.nonoxy.residetrack.feature.splash.presentation.di.featureSplashPresentationModule
 import dev.nonoxy.residetrack.feature.add_room.impl.di.featureAddRoomImplModule
 import dev.nonoxy.residetrack.feature.add_room.presentation.di.featureAddRoomPresentationModule
@@ -29,6 +30,7 @@ fun initKoin(appDeclaration: KoinAppDeclaration) {
             coreMVIKotlinModule,
             coreDatabaseModule,
             coreInitializerModule,
+            coreRoomsModule,
 
             featureSplashPresentationModule,
 
